a472e09e8f253c8e18b4f1eed6e5aab8cdbc0cbf
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2024-07-29 14:00:12 +1200

    Update toki_ilo.js

--- toki_ilo.js
+++ toki_ilo.js
@@ -98,7 +98,7 @@
     _pj = {};
     _pj_snippets(_pj);
     function nnptosp(nnp){
-        if(nnp == "WAWA" || nnp == "LA" || nnp == "ALA"){
+        if(nnp == "WAWA" || nnp == "LA" || nnp == "ALA" || nnp == "LAWA"){
             return(nnp)
         }
         var output = ""
